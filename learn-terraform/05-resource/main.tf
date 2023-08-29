b8504355a1ae836a7c917c6cc074f3f20f3a007c
favorgithub <gloify@yahoo.com>
2023-08-29 14:17:36 -0500

    commit files

--- learn-terraform/05-resource/main.tf
+++ learn-terraform/05-resource/main.tf
@@ -1,13 +1,63 @@
-resource "aws_instance" "web" {
-  instance_type = "t3.micro"
-  ami = "ami-053b0d53c279acc90"
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_vpc" "main" {
+  cidr_block = "172.16.0.0/16"
+  instance_tenancy = "default"
   tags = {
-    Name = "Hello World2"
-
+    Name = "main"
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
+
+#Create security group with firewall rules
+resource "aws_security_group" "jenkins-sg-2025" {
+  name        = var.security_group
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound from Jenkins server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+}
+
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins-sg-2025.id]
+  tags= {
+    Name = var.tag_name
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myElasticIP" {
+  domain      = "vpc"
+  instance = aws_instance.myFirstInstance.id
+  tags= {
+    Name = "jenkins_elastic_ip"
+  }
 }
