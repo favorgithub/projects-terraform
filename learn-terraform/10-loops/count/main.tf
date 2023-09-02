4747e2ddd68d2bdab62cfe99006dd5343f14bcd3
favorgithub <gloify@yahoo.com>
2023-09-02 10:03:07 -0500

    commit files

--- learn-terraform/10-loops/count/main.tf
+++ learn-terraform/10-loops/count/main.tf
@@ -3,16 +3,16 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "web" {
-  ami = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
-  count = 2
+#resource "aws_instance" "web" {
+#  ami = "ami-053b0d53c279acc90"
+#  instance_type = "t2.micro"
+#  count = 2
+#
+#  tags = {
+#    Name = "Terraform ec2"
+#  }
 
-  tags = {
-    Name = "Terraform ec2"
-  }
 
-}
 resource "aws_instance" "web2" {
   count         = length(var.instances)
   ami           = "ami-053b0d53c279acc90"
